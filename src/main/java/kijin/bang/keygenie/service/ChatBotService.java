26b40bda2d2f9ee62dc330106ac35a30eddeb72d
KijinBANG <mr.kijin.bang@gmail.com>
2022-04-28 12:13:30 +0900

    add ChatBot

---
+++ src/main/java/kijin/bang/keygenie/service/ChatBotService.java
@@ -0,0 +1,9 @@
+package kijin.bang.keygenie.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface ChatBotService {
+	public String download(HttpServletRequest request, HttpServletResponse response);
+
+}
